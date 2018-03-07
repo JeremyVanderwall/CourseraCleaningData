684d025a5fd68cf95e738ec2e574c157b2ed3455
Unknown <jvander2@ncsu.edu>
2018-03-07 15:54:11 -0500

    work

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CourseraCleaningData
 Coursera getting and cleaning data project
+
+Steps in Tiding data
+1) Download the data
+2) Merge training and testing sets
+3) Select only needed columns
+4) Properly label columns
+5)
--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,18 @@
+#download the data
+FileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(FileURL, destfile = "courseraTemp.zip", mode = "wb")
+unzip("courseraTemp.zip")
 
+#load data into dataframes
+testData <- read.csv(".\\UCI HAR Dataset\\test\\X_test.txt",sep = " ", header = FALSE)
+testLabels <- read.csv(".\\UCI HAR Dataset\\test\\y_test.txt",sep = " ", header = FALSE)
+subjectTestData <- read.csv(".\\UCI HAR Dataset\\test\\subject_test.txt",sep = " ", header = FALSE)
+testData <- cbind(subjectTestData, testData)
+
+trainData <- read.csv(".\\UCI HAR Dataset\\train\\X_train.txt",sep = " ", header = FALSE)
+subjectTrainData <- read.csv(".\\UCI HAR Dataset\\train\\subject_train.txt",sep = " ", header = FALSE)
+trainData <- cbind(subjectTrainData, trainData)
+
+Header <- read.csv(".\\UCI HAR Dataset\\features.txt",sep = " ", header = FALSE)
+names(trainData) <- Header$V2
+fullData <- rbind(testData, trainData)
